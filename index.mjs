88801c9e42fb95060dbb49980cd0d0b57ebbb342
Joey Guerra <joey@joeyguerra.com>
2021-04-03 20:54:55 -0500

    create a server

--- index.mjs
+++ index.mjs
@@ -1,1 +1,11 @@
-console.log(process.argv);
+import http from "http";
+
+const requestListener = (req, res) => {
+    res.statusCode = 200;
+    res.write("Ok - Shipping");
+    res.end();
+}
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT || 3000, backlog => {
+    console.log(`http://localhost:${process.env.PORT || 3000}`);
+});
